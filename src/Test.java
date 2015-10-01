d9782c52cc28d9817aec6d7abf3e54bf6e746fb1
ahdieh <aemadi@my.centennialcollege.ca>
2015-10-01 02:38:45 -0700

    Create one question and check it using arrays

---
+++ src/Test.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+import java.security.SecureRandom;
+
+
+public class Test {
+	
+	// PRIVATE INSTANCE VARIABLES
+    private int _answere;
+    
+	Scanner input = new Scanner(System.in);
+    private static final SecureRandom randomNumbers = new SecureRandom();
+
+
+        
+    String[] RightAnsweres = {"Excellent!","Good!","Keep up the good work!","Nice work!","Amazing!","Good job!"};
+    private int _rightAnsweres = RightAnsweres.length;
+    String[] WrongAnsweres = {"No. Please try again","Wrong. Try once more","Don't give up!","No. Keep trying..."};
+    private int _wrongAnsweres =WrongAnsweres.length;
+    
+    // PUBLIC METHODE
+    
+    public void simulateQuestion(){
+    	System.out.println("Question 1: Which keyword in a class declaration is followed immediately by the class’s name?");
+		System.out.println("1. public");
+		System.out.println("2. private");
+		System.out.println("3. class");
+		System.out.println("4. void");
+
+    }
+    
+    public void checkAnswer(){
+			
+    		try{
+ 			System.out.print("Answere: ");
+			_answere = input.nextInt();
+			int number;
+			if (_answere<0){
+    			System.out.println("You entered invalid data! Please enter a positive number");
+			}
+			else{
+				if (_answere>4){
+        			System.out.println("You entered invalid data! Please enter a correct number");
+				}
+				else{
+			switch(_answere){
+			case 3:
+			    number = randomNumbers.nextInt(_rightAnsweres);
+				System.out.println(RightAnsweres[number]);
+				break;
+			
+			case 1:
+			case 2:
+			case 4:
+				 number = randomNumbers.nextInt(_wrongAnsweres);
+				System.out.println(WrongAnsweres[number]);
+				break;
+			}
+			}
+			}
+    		} catch (Exception e) {
+    			System.out.println("You entered invalid data! Please enter a number");
+    		}
+    	}	
+}

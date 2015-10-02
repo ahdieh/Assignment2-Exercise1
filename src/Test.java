3586a941926f0c1d342a6e869947fb078d747ba2
ahdieh <aemadi@my.centennialcollege.ca>
2015-10-01 22:37:05 -0700

    Make checkAnswer method to void and some changes, Create inputAnswer and simulateMessage methods, and Create getters and setters.

--- src/Test.java
+++ src/Test.java
@@ -4,20 +4,98 @@ import java.security.SecureRandom;
 
 public class Test {
 	
-	// PRIVATE INSTANCE VARIABLES
-    private int _answere;
-    
+	// PRIVATE INSTANCE VARIABLES +++++++++++++++++++++++++++++++++++++
+    private  String _answere;
+    private int _random;
+	private int _wrong;
+	private int _right;
+	private String _print;
 	Scanner input = new Scanner(System.in);
     private static final SecureRandom randomNumbers = new SecureRandom();
 
 
         
     String[] RightAnsweres = {"Excellent!","Good!","Keep up the good work!","Nice work!","Amazing!","Good job!"};
-    private int _rightAnsweres = RightAnsweres.length;
-    String[] WrongAnsweres = {"No. Please try again","Wrong. Try once more","Don't give up!","No. Keep trying..."};
-    private int _wrongAnsweres =WrongAnsweres.length;
+    String[] WrongAnsweres = {"No. Please try again","Wrong. Try the next","Don't give up!","No. Keep trying..."};
     
-    // PUBLIC METHODE
+ 
+    //PROPERTIES ++++++++++++++++++++++++++++++++++++++++++++++++++++
+    public String GetAnswrere(){
+    	return this._answere;
+    }
+    
+    public void SetAnswere(String answere){
+    	
+    	try {
+			int a = Integer.parseInt(answere);
+			
+			if(a < 1 || a> 4) {
+				System.out.println("You entered an invalid number");
+				this._answere = answere;
+			}
+			else {
+				this._answere = answere;
+			}
+		} catch (Exception exception) {
+			System.out.println("Please enter an integer value");
+		}
+    }
+    
+    public int GetRandom(){
+    	return this._random;
+    }
+    
+    public void SetRandom(int random){
+    	
+    	this._random = random;
+    }
+    
+    public String GetPrint(){
+    	return this._print;
+    }
+    
+    public void SetPrint(String print){
+    	
+    	this._print = print;
+    }
+    
+    public int GetRightt(){
+    	return this._right;
+    }
+    
+    public void SetRight(int right){
+    	
+    	this._right = right;
+    }
+    
+    public int GetWrong(){
+    	return this._wrong;
+    }
+    
+    public void SetWrong(int wrong){
+    	
+    	this._wrong = wrong;
+    }
+
+ // CONSTRUCTOR ++++++++++++++++++++++++++++++++++++++++++++++++
+    public Test(){
+			this._initialize("0" , 0 , 0 , 0 , "");
+
+   }
+    
+	// PRIVATE METHODS ++++++++++++++++++++++++++++++++++++++
+
+	 private void _initialize(String answere, int random , int wrong, int right, String print) {
+			this.SetAnswere(answere);
+			this.SetRandom(random);
+			this.SetWrong(wrong);
+			this.SetRight(right);
+			this.SetPrint(print);
+
+	 }
+    
+   
+    // PUBLIC METHODE ++++++++++++++++++++++++++++++++++++++++++++++++++
     
     public void simulateQuestion(){
     	System.out.println("Question 1: Which keyword in a class declaration is followed immediately by the class’s name?");
@@ -28,36 +106,37 @@ public class Test {
 
     }
     
-    public String checkAnswer(){
-			
-    		try{
- 			System.out.print("Answere: ");
-			_answere = input.nextInt();
-			int number;
-			if (_answere<0){
-    			return("You entered invalid data! Please enter a positive number");
-			}
-			else{
-				if (_answere>4){
-        			return("You entered invalid data! Please enter a correct number");
-				}
-				else{
-			switch(_answere){
-			case 3:
-			    number = randomNumbers.nextInt(_rightAnsweres);
-				return(RightAnsweres[number]);
-			case 1:
-			case 2:
-			case 4:
-				 number = randomNumbers.nextInt(_wrongAnsweres);
-				return(WrongAnsweres[number]);
-			}
-			}
-			}
-    		} catch (Exception e) {
-    			return("You entered invalid data! Please enter a number");
-    		}
-			return null;
-
-    	}	
+    public void inputAnswer(){
+    	
+    	System.out.print("Answere: ");
+		this._answere = input.nextLine();
+		this.SetAnswere(this._answere);
+    }
+    
+    
+    public void checkAnswer(int answere){
+    	
+    	        switch(answere){
+			        case 1:
+		            case 2:
+		            case 4:
+			         this.SetRandom(randomNumbers.nextInt(this.WrongAnsweres.length));
+				     String c = this.WrongAnsweres[this.GetRandom()];
+				     this.SetPrint(c);
+				     break;
+				    
+		            case 3:
+					 this.SetRandom(randomNumbers.nextInt(this.RightAnsweres.length));
+				     String d = this.RightAnsweres[this.GetRandom()];
+				     this.SetPrint(d);
+				     break;
+			         }
+			    }
+    
+		public void simulateMessage(){
+			System.out.println();
+			System.out.println(this.GetPrint());
+		}
+    		
 }
+

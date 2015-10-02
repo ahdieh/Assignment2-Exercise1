23471496c4f9ac77004f1611db88602ef20adfb9
ahdieh <aemadi@my.centennialcollege.ca>
2015-10-02 02:50:27 -0700

    Modifying program to create 4 questions

--- src/Program.java
+++ src/Program.java
@@ -1,14 +1,21 @@
+import java.security.SecureRandom;
+import java.util.*;
+
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Test test = new Test();
-		test.simulateQuestion();
-		test.inputAnswer();
-		test.checkAnswer(Integer.parseInt(test.GetAnswrere()));
-		test.simulateMessage();
-		test.Result();
+		for(int i=0;i<4;i++){
+			test.SetQuestionNumber(i);
+		    test.simulateQuestion();
+		    test.inputAnswer();
+		    test.checkAnswer(Integer.parseInt(test.GetAnswrere()));
+		    test.simulateMessage();
+		   		    
+		}
+		 test.Result();
 	}
 
 }
--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.lang.reflect.Array;
 import java.security.SecureRandom;
 
 
@@ -10,16 +12,33 @@ public class Test {
 	private int _wrong;
 	private int _right;
 	private String _print;
+	private int _questionNumber;
+	private int _correctAns;
 	Scanner input = new Scanner(System.in);
     private static final SecureRandom randomNumbers = new SecureRandom();
 
 
         
-    String[] RightAnsweres = {"Excellent!","Good!","Keep up the good work!","Nice work!","Amazing!","Good job!"};
+   // String[] RightAnsweres = {"Excellent!","Good!","Keep up the good work!","Nice work!","Amazing!","Good job!"};
     String[] WrongAnsweres = {"No. Please try again","Wrong. Try the next","Don't give up!","No. Keep trying..."};
     
  
     //PROPERTIES ++++++++++++++++++++++++++++++++++++++++++++++++++++
+    
+    public int GetQuestionNumber(){
+    	return this._questionNumber;
+    }
+    public void SetQuestionNumber(int questionNumber){
+    	this._questionNumber = questionNumber;
+    }
+    
+    public int GetCorrectAns(){
+    	return this._correctAns;
+    }
+    public void SetCorrectAns(int correctAns){
+    	this._correctAns = correctAns;
+    }
+    
     public String GetAnswrere(){
     	return this._answere;
     }
@@ -37,7 +56,7 @@ public class Test {
 				this._answere = answere;
 			}
 		} catch (Exception exception) {
-			System.out.println("Please enter an integer value");
+			System.out.println("You did not enter an integer value");
 		}
     }
     
@@ -97,14 +116,50 @@ public class Test {
    
     // PUBLIC METHODE ++++++++++++++++++++++++++++++++++++++++++++++++++
     
+	 
+	 // Create Questions ++++++++++++++++++++++++++++++++++++++++++++++++++++
     public void simulateQuestion(){
-    	System.out.println("Question 1: Which keyword in a class declaration is followed immediately by the class’s name?");
-		System.out.println("1. public");
-		System.out.println("2. private");
-		System.out.println("3. class");
-		System.out.println("4. void");
-
+    	String[] Questions = new String[4];
+    	Questions[0]= "***Question 1: Which keyword in a class dtion is followed immediatelyeclara by the class’s name?";
+    	Questions[1]= "***Question 2: Which statement is false?";
+    	Questions[2]= "***Question 3: Empty parentheses following a method name in a method declaration indicate that the method does not require any parameters to perform its task. \n  if (c => 7) \n  System.out.println(\"c is equal to or greater than 7\"); ";
+    	Questions[3]= "***Question 4: The key logical units of the computer are:";
+    	System.out.println(Questions[this.GetQuestionNumber()]);
+    	switch(this.GetQuestionNumber()){
+    	case 0:
+    		System.out.println("1. public");
+    		System.out.println("2. private");
+    		System.out.println("3. class");
+    		System.out.println("4. void");
+    		this.SetCorrectAns(3);
+    		break;
+    	case 1:
+    		System.out.println("1. Return type viod indicates that a method will not return a value. ");
+    		System.out.println("2.  By convention, method names begin with an uppercase first letter, and all subsequent words in the name begin with a capital first letter. ");
+    		System.out.println("3.  An import declaration is not required when one class in a package uses another in the same package. ");
+    		System.out.println("4. Empty parentheses following a method name in a method declaration indicate that the method does not require any parameters to perform its task. Q2");
+    		this.SetCorrectAns(2);
+    		break;
+    	case 2:
+    		System.out.println("1. =>");
+    		System.out.println("2. parentheses after if");
+    		System.out.println("3. missing ; after if");
+    		System.out.println("4. none of the above");
+    		this.SetCorrectAns(1);
+    		break;
+    	case 3:
+    		System.out.println("1.  input unit, output unit");
+    		System.out.println("2.  central processing unit, arithmetic and logic unit");
+    		System.out.println("3. memory unit, secondary storage unit");
+    		System.out.println("4. All of them");
+    		this.SetCorrectAns(4);
+    		break;
+    		
+    	}
+		
     }
+    
+    // Input answers +++++++++++++++++++++++++++++++++++++++++++++++++++++++
     
     public void inputAnswer(){
     	
@@ -113,45 +168,67 @@ public class Test {
 		this.SetAnswere(this._answere);
     }
     
+    // Checking answers ++++++++++++++++++++++++++++++++++++++++++++++++++++
     
     public void checkAnswer(int answere){
     	
-    	        switch(answere){
-			        case 1:
-		            case 2:
-		            case 4:
-			         this.SetRandom(randomNumbers.nextInt(this.WrongAnsweres.length));
-				     String c = this.WrongAnsweres[this.GetRandom()];
-				     this.SetPrint(c);
-				     int w =this.GetWrong();
-				     w++;
-				     this.SetWrong(w);
-				     break;
-				    
-		            case 3:
-					 this.SetRandom(randomNumbers.nextInt(this.RightAnsweres.length));
-				     String d = this.RightAnsweres[this.GetRandom()];
-				     this.SetPrint(d);
-				     int r =this.GetRight();
-				     r++;
-				     this.SetRight(r);
-				     break;
-			         }
+    	if (answere == this.GetCorrectAns()){
+		     int r =this.GetRight();
+		     r++;
+		     this.SetRight(r);
+    	}
+    	else{
+    		this.SetRandom(randomNumbers.nextInt(this.WrongAnsweres.length));
+		     String c = this.WrongAnsweres[this.GetRandom()];
+		     this.SetPrint(c);
+		     int w =this.GetWrong();
+		     w++;
+		     this.SetWrong(w);
+    	}
+    	
 			    }
+    
+    // Printing related messages +++++++++++++++++++++++++++++++++++++++++++++
     
 		public void simulateMessage(){
 			System.out.println();
+	    	if (Integer.parseInt(this.GetAnswrere()) == this.GetCorrectAns()){
+	    		//this.SetRandom(randomNumbers.nextInt(this.RightAnsweres.length));
+			  //   String d = this.RightAnsweres[this.GetRandom()];
+			     //this.SetPrint(d);
+	    		switch (randomNumbers.nextInt(4)){
+	    		case 0:
+	    			this.SetPrint("Excellent!");
+	    		case 1:
+	    			this.SetPrint("Good!");
+	    		case 2:
+	    			this.SetPrint("Keep up the good work!");
+	    		case 3:
+	    			this.SetPrint("Amazing!");
+
+
+	    		}
+	    	}
+	    	
+	    	else{
+	    		this.SetRandom(randomNumbers.nextInt(this.WrongAnsweres.length));
+			     String c = this.WrongAnsweres[this.GetRandom()];
+			     this.SetPrint(c);
+	    	}
+	    	
 			System.out.println(this.GetPrint());
+			System.out.println();
 		}
     	
+		// Printing results +++++++++++++++++++++++++++++++++++++++++++++++++++++
+		
 		public void Result(){
-			int total = this.GetRight()+this.GetWrong();
-			double percentage =((this.GetRight()/total)*100);
-			System.out.printf("***You answered %d correct and %d wrong.\nYour correct answeres are %.2f percent of the total questions", this.GetRight(),this.GetWrong(), percentage);
+			System.out.printf("***You answered %d correct and %d wrong.\nYour correct answeres are %d percent of the total questions", this.GetRight(),this.GetWrong(), ((this.GetRight()*100)/(this.GetRight()+this.GetWrong())));
 			
 	
 			
 		}
+
 }
 
 
